935476acd7b715ac6706651f8a5ae7aafb325258
Spencer <spencerchase1991@gmail.com>
2019-05-16 14:47:37 -0600

    added APIs

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
-exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-};
+export const spotifyKeys = {
+    id: '7a7e03c09c1647c1b6f6c7103b875c74',
+    secret: '81e9ef6d90c64594b23ca8014c187da4'
+}
